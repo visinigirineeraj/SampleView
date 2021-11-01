8c621299611711d9912c4ba9f8f7c185a6c21177
visinigirineeraj <visinigirineeraj@gmail.com>
2021-11-01 17:10:13 +0530

    First Commit

---
+++ SAP/Features/ImagePciker/Controllers/ImagePickerViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  SAP
+//
+//  Created by byndr on 01/11/21.
+//
+
+import UIKit
+
+class ImagePickerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
